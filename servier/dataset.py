5c4050ff2e1725b9ad323d885b9a369650595b00
Hugo Defois <hugo.defois@symphonyretailai.com>
2021-10-18 21:26:50 +0200

    fix(dataset): remove the train/test split and add cross validation

--- servier/dataset.py
+++ servier/dataset.py
@@ -2,13 +2,18 @@ import pandas as pd
 import numpy as np
 import pickle as pk
 from rdkit import Chem
-from sklearn.model_selection import train_test_split
-from servier.feature_extractor import fingerprint_features
-
+from feature_extractor import fingerprint_features
 
 TEST_RATIO = 0.2
-RANDOM_SMILES = 1
+RANDOM_SMILES = 60
 USEFUL_FEATURES_PATH = "__save__/useful_features.pkl"
+SMILES_CHARS = [' ', '#', '%', '(', ')', '+', '-', '.', '/',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                '9', '=', '@', 'A', 'B', 'C', 'F', 'H', 'I',
+                'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T',
+                'V', 'X', 'Z', '[', '\\', ']', 'a', 'b', 'c',
+                'e', 'g', 'i', 'l', 'n', 'o', 'p', 'r', 's',
+                't', 'u', '!', 'E']
 
 
 def randomize_smiles(smiles):
@@ -19,45 +24,52 @@ def randomize_smiles(smiles):
     return Chem.MolToSmiles(nm, canonical=False, isomericSmiles=False)
 
 
-def upsample_dataset(X, y):
+def upsample_dataset(X, y, groups):
     X_upsampled = X.copy()
     y_upsampled = y.copy()
-    for smiles, y_score in zip(X, y):
+    for j, (smiles, y_score) in enumerate(zip(X, y)):
         for i in range(RANDOM_SMILES):
             random_smiles = randomize_smiles(smiles)
+            groups.append(groups[j])
             X_upsampled.append(random_smiles)
             y_upsampled.append(y_score)
     unique_smiles, unique_indexes = np.unique(X_upsampled, return_index=True)
     X_upsampled = np.array(X_upsampled)[unique_indexes]
     y_upsampled = np.array(y_upsampled)[unique_indexes]
-    return X_upsampled, y_upsampled
+    return X_upsampled, y_upsampled, groups
 
 
-def balance_dataset(X, y):
+def balance_dataset(X, y, groups):
     minority = 0 if sum(y) > int(len(X) / 2) else 1
     keep_elements = len(y[y == minority])
-    X_balanced = np.append(X[y == minority][:keep_elements],
-                           X[y != minority][:keep_elements])
-    y_balanced = np.append(y[y == minority][:keep_elements],
-                           y[y != minority][:keep_elements])
-    return X_balanced, y_balanced
+    minority_indices = np.random.randint(len(y[y == minority]),
+                                         size=keep_elements)
+    majority_indices = np.random.randint(len(y[y != minority]),
+                                         size=keep_elements)
+    groups = np.append(groups[y == minority][minority_indices],
+                       groups[y != minority][majority_indices])
+    X_balanced = np.append(X[y == minority][minority_indices],
+                           X[y != minority][majority_indices])
+    y_balanced = np.append(y[y == minority][minority_indices],
+                           y[y != minority][majority_indices])
+    return X_balanced, y_balanced, groups
 
 
 def load_existing_dataset():
-    with open('../vectors_save.pkl', 'rb') as f:
-        (X_train, y_train, X_test, y_test) = pk.load(f)
+    with open('__save__/vectors_save.pkl', 'rb') as f:
+        (X_train, y_train, groups) = pk.load(f)
     print("Dataset loaded from existing file.")
-    return X_train, X_test, y_train, y_test
+    return X_train, y_train, groups
 
 
-def save_dataset(X_train, y_train, X_test, y_test):
-    with open('../vectors_save.pkl', 'wb') as f:
-        data = X_train, y_train, X_test, y_test
+def save_dataset(X_train, y_train, groups):
+    with open('__save__/vectors_save.pkl', 'wb') as f:
+        data = X_train, y_train, groups
         pk.dump(data, f)
     print("Dataset saved.")
 
 
-def filter_features(X_train, X_test=None, load_features=False):
+def filter_features(X_train, load_features=False):
     if load_features:
         useful_features = pk.load(open(USEFUL_FEATURES_PATH, "rb"))
     else:
@@ -65,53 +77,65 @@ def filter_features(X_train, X_test=None, load_features=False):
         useful_features = [i for i, k in enumerate(unuseful_features) if k]
         pk.dump(useful_features, open(USEFUL_FEATURES_PATH, "wb"))
     X_train = np.delete(X_train, useful_features, axis=1)
-    if X_test:
-        X_test = np.delete(X_test, useful_features, axis=1)
-        return X_train, X_test
-    else:
-        return X_train
+    return X_train
 
 
-def keep_unique_samples(X, y):
+def keep_unique_samples(X, y, groups):
     unique_values, unique_indexes = np.unique(X, axis=0, return_index=True)
     X = np.array(X)[unique_indexes]
     y = np.array(y)[unique_indexes]
-    return X, y
+    groups = np.array(groups)[unique_indexes]
+    return X, y, groups
 
 
-def build_training_dataset(radius, size, save=False,
-                           use_save=False, path="ds.csv"):
+def smiles2vec(X, vec_size):
+    char_to_int = {v: i for i, v in enumerate(SMILES_CHARS)}
+    one_hot = np.zeros((X.shape[0], vec_size,
+                       len(char_to_int)), dtype=np.int8)
+    for i, smile in enumerate(X):
+        one_hot[i, 0, char_to_int["!"]] = 1
+        for j, c in enumerate(smile):
+            one_hot[i, j+1, char_to_int[c]] = 1
+        one_hot[i, len(smile)+1:, char_to_int["E"]] = 1
+    return one_hot
+
+
+def build_training_dataset(radius, size, save=False, use_save=False,
+                           path="ds.csv", model=1):
     if use_save:
         return load_existing_dataset()
 
     print("Loading and preprocessing the dataset...")
 
     raw_dataset = pd.read_csv(path, sep=",")
-    X_train, X_test, y_train, y_test = train_test_split(raw_dataset["smiles"],
-                                                        raw_dataset["P1"],
-                                                        test_size=TEST_RATIO,
-                                                        random_state=0,
-                                                        shuffle=True)
-    X_train, y_train = upsample_dataset(X_train.tolist(), y_train.tolist())
-    X_test, y_test = upsample_dataset(X_test.tolist(), y_test.tolist())
-    X_train, y_train = keep_unique_samples(X_train, y_train)
-    X_train, y_train = balance_dataset(X_train, y_train)
-    X_train = [list(fingerprint_features(x, radius, size)) for x in X_train]
-    X_test = [list(fingerprint_features(x, radius, size)) for x in X_test]
-    X_train, X_test = filter_features(X_train, X_test)
+    X_train = raw_dataset["smiles"]
+    y_train = raw_dataset["P1"]
+    groups = list(range(len(X_train)))
+    X_train, y_train, groups = upsample_dataset(X_train.tolist(),
+                                                y_train.tolist(),
+                                                groups)
+    X_train, y_train, groups = keep_unique_samples(X_train, y_train, groups)
+    X_train, y_train, groups = balance_dataset(X_train, y_train,
+                                               groups)
+    if model == 1:
+        X_train = [list(fingerprint_features(x, radius, size)) for x in X_train]
+        X_train = filter_features(X_train)
+    else:
+        vec_size = len(max(X_train, key=len)) + 1
+        X_train = smiles2vec(X_train, vec_size)
+
     print("Dataset built.")
 
-    X_train, y_train, X_test, y_test = [np.array(arr) for arr in [
+    X_train, y_train, groups = [np.array(arr) for arr in [
                                             X_train,
                                             y_train,
-                                            X_test,
-                                            y_test
+                                            groups
                                         ]]
 
     if save:
-        save_dataset(X_train, y_train, X_test, y_test)
+        save_dataset(X_train, y_train, groups)
 
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train, groups
 
 
 def build_evaluation_dataset(radius, size, path="../dataset_single.csv"):
--- servier/model.py
+++ servier/model.py
@@ -2,13 +2,15 @@ import warnings
 warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
 import kerastuner as kt
 import tensorflow as tf
+import numpy as np
 import pickle
 import os
-from tensorflow.keras.layers import Dense, Dropout
+from sklearn.model_selection import GroupKFold
+from keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
+from keras.callbacks import EarlyStopping
+from keras.layers import Input
+from keras.models import Model
 from servier.dataset import filter_features
 from servier.feature_extractor import fingerprint_features
 
@@ -17,25 +19,39 @@ HYPERPARAMETERS_PATH = "__save__/best_hyperparameters.pkl"
 PARAMETERS_PATH = "__save__/best_parameters"
 
 
-class Tuner(kt.engine.tuner.Tuner):
-    def run_trial(self, trial, X_train, y_train, X_test=[], y_test=[],
-                  batch_size=1, epochs=1, callbacks=None):
-        model = self.hypermodel.build(trial.hyperparameters)
-        hist = model.fit(X_train, y_train,
-                         validation_data=(X_test, y_test),
-                         epochs=epochs,
-                         batch_size=batch_size,
-                         callbacks=callbacks)
-        loss = [hist.history[k][-1] for k in hist.history]
-        loss = {k: loss[i] for i, k in enumerate(hist.history.keys())}
-        self.oracle.update_trial(trial.trial_id, loss)
+# For adding new activation function
+from keras import backend as K
+from keras.utils.generic_utils import get_custom_objects
+
+def swish(x):
+    return (K.sigmoid(x) * x)
+
+get_custom_objects().update({'swish': Activation(swish)})
+
+class CVTuner(kt.engine.tuner.Tuner):
+    def run_trial(self, trial, X, y, splits, batch_size=32, epochs=1, callbacks=None):
+        val_losses = []
+        for train_indices, test_indices in splits:
+            X_train, X_test = X[train_indices], X[test_indices]
+            y_train, y_test = y[train_indices], y[test_indices]
+            model = self.hypermodel.build(trial.hyperparameters)
+            hist = model.fit(X_train,y_train,
+                      validation_data=(X_test,y_test),
+                      epochs=epochs,
+                        batch_size=batch_size,
+                      callbacks=callbacks)
+            val_losses.append([hist.history[k][-1] for k in hist.history])
+        val_losses = np.asarray(val_losses)
+        self.oracle.update_trial(trial.trial_id, {k:np.mean(val_losses[:,i]) for i,k in enumerate(hist.history.keys())})
         self.save_model(trial.trial_id, model)
 
 
 def build_model(hp, input_shape=0):
     input = Input(shape=(hp.Int('nb_cols', input_shape, input_shape),))
-    x = Dense(hp.Int('num_units_0', 4, 64), activation="relu")(input)
-    x = Dropout(hp.Float('dropout', 0.0, 0.3))(x)
+    x = Dense(hp.Int('num_units_0', 4, 64), activation=hp.Choice('act_0', ["relu", "swish"]))(input)
+    x = Dropout(hp.Float('dropout_0', 0.0, 0.3))(x)
+    x = Dense(hp.Int('num_units_1', 4, 64), activation=hp.Choice('act_1', ["relu", "swish"]))(x)
+    x = Dropout(hp.Float('dropout_1', 0.0, 0.3))(x)
     output = Dense(1, activation='sigmoid')(x)
     model = Model(inputs=input, outputs=output)
     model.compile(loss="binary_crossentropy",
@@ -44,19 +60,20 @@ def build_model(hp, input_shape=0):
     return model
 
 
-def tune_model(X_train, y_train, X_test, y_test):
-    tuner = Tuner(
+def tune_model(X_train, y_train, groups):
+    group_kfold = GroupKFold(n_splits=4)
+    folds = list(group_kfold.split(X_train, y_train, groups))
+    tuner = CVTuner(
         hypermodel=lambda hp: build_model(hp, input_shape=X_train.shape[1]),
         oracle=kt.oracles.BayesianOptimization(
-            objective=kt.Objective('val_acc', direction='max'),
+            objective=kt.Objective('val_accuracy', direction='max'),
             max_trials=10
         ),
         directory=os.path.normpath('C:/'),
         overwrite=True
     )
-    tuner.search(X_train, y_train, X_test=X_test,
-                 y_test=y_test, batch_size=128, epochs=1,
-                 callbacks=[EarlyStopping('val_acc', mode='max', patience=4)])
+    tuner.search(X_train, y_train, splits=folds, batch_size=128, epochs=50,
+                 callbacks=[EarlyStopping('val_accuracy', mode='max', patience=4)])
     best_hps = tuner.get_best_hyperparameters()[0]
     pickle.dump(best_hps, open(HYPERPARAMETERS_PATH, "wb"))
     return tuner, best_hps
@@ -64,7 +81,7 @@ def tune_model(X_train, y_train, X_test, y_test):
 
 def train_model(X_train, y_train, tuner, best_hps):
     hypermodel = tuner.hypermodel.build(best_hps)
-    hypermodel.fit(X_train, y_train, batch_size=128, epochs=1)
+    hypermodel.fit(X_train, y_train, batch_size=128, epochs=300)
     hypermodel.save_weights(PARAMETERS_PATH)
     return hypermodel
 
